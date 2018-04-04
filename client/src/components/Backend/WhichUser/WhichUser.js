ea6dd2c67a584f1b36f14a39d69e504c7142aea7
Edwin Martinez <kastille84@gmail.com>
2018-04-04 16:29:43 -0400

    Added WhichUser logic

--- client/src/components/Backend/WhichUser/WhichUser.js
+++ client/src/components/Backend/WhichUser/WhichUser.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import { connect } from 'react-redux';
 
 class WhichUser extends Component {
 
@@ -9,6 +10,20 @@ class WhichUser extends Component {
                 <div className="row">
                     <div className="col-md-6 offset-md-3 col-sm-8 offset-sm-2">
                         <h2>Who is using Stint?</h2>
+                        <div>
+                            <form>
+                                <div className="form-group">
+                                    <label>{this.props.userRedux.user.name}</label>
+                                    <input 
+                                        type="text"
+                                        maxLength="4"
+                                        placeholder="Enter Your Pin"
+                                         />
+                                </div>
+                            </form>                        
+                        </div>
+                        <hr/>
+
                     </div>
                 </div>
             </div>
@@ -16,4 +31,10 @@ class WhichUser extends Component {
     }
 }
 
-export default WhichUser;
+const mapStateToProps = (state) => {
+    return {
+        userRedux: state.userRedux
+    }
+}
+
+export default connect(mapStateToProps)(WhichUser);
